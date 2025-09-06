4d744e0debed0a4c5dc30e3977c9136bd02b0764
Antonio <antoshka2003@bk.ru>
2025-09-06 19:17:58 +0300

    practical_tasks: create test site from free template

--- project/project/settings.py
+++ project/project/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'django.contrib.sites',
     'django.contrib.flatpages',
+    'fpages'
 ]
 
 SITE_ID = 1
@@ -126,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [BASE_DIR / 'static',]
