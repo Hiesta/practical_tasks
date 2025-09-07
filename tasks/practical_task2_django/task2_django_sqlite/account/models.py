51c45434111a417c276c29251a1321dd5400dd13
Antonio <antoshka2003@bk.ru>
2025-09-07 22:32:04 +0300

    practical_tasks: db migrations

--- tasks/practical_task2_django/task2_django_sqlite/account/models.py
+++ tasks/practical_task2_django/task2_django_sqlite/account/models.py
@@ -15,16 +15,6 @@ POSITIONS = [
 ]
 
 
-# Create your models here.
-class Order(models.Model):
-    pass
-
-
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.FloatField(default=0.0)
-
-
 class Staff(models.Model):
     full_name = models.CharField(max_length=255)
     position = models.CharField(max_length=2,
@@ -33,7 +23,24 @@ class Staff(models.Model):
     labor_contract = models.IntegerField()
 
 
-class ProductOrder(models.Model):
-    pass
+# Create your models here.
+class Order(models.Model):
+    time_in = models.DateTimeField(auto_now_add=True)
+    time_out = models.DateTimeField(null=True)
+    cost = models.FloatField(default=0.0)
+    pickup = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False)
+    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    products = models.ManyToManyField('Product', through='ProductOrder')
 
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField(default=0.0)
+
+
+class ProductOrder(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=1)
 
